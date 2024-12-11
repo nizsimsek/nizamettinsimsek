01903fbcadf59e6d418480123baaf38bb2a9e1b3
Nizamettin Şimşek <nizsimsek@icloud.com>
2024-12-11 23:38:13 +0300

    Projects UI/Ux

--- src/components/project-card.tsx
+++ src/components/project-card.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import { ProjectInfo } from "@/lib/types";
 import { motion, useScroll, useTransform } from "framer-motion";
 import { useRef } from "react";
+import { FaExternalLinkAlt } from "react-icons/fa";
 
 type ProjectProps = ProjectInfo;
 
@@ -10,6 +11,7 @@ export default function Project({
   description,
   tags,
   imageUrl,
+  link,
 }: ProjectProps) {
   const ref = useRef<HTMLDivElement>(null);
   const { scrollYProgress } = useScroll({
@@ -19,6 +21,10 @@ export default function Project({
   const scaleProgress = useTransform(scrollYProgress, [0, 1], [0.8, 1]);
   const opacityProgress = useTransform(scrollYProgress, [0, 1], [0.6, 1]);
 
+  const openWebsiteNewPage = (url: string) => {
+    window.open(url, "_blank");
+  };
+
   return (
     <motion.div
       ref={ref}
@@ -26,11 +32,15 @@ export default function Project({
         scale: scaleProgress,
         opacity: opacityProgress,
       }}
-      className="group mb-3 sm:mb-8 last:mb-0"
+      className="group mb-3 sm:mb-8 last:mb-0 hover:cursor-pointer"
+      onClick={() => openWebsiteNewPage(link)}
     >
       <section className="bg-white max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative hover:bg-gray-200 transition sm:group-even:pl-8 dark:text-white dark:bg-white/10 dark:hover:bg-white/20">
         <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-72">
-          <h3 className="text-2xl font-semibold">{title}</h3>
+          <h3 className="flex gap-1 text-2xl w-fit font-semibold items-center border-b-2 border-b-white/0 group-hover:border-b-black dark:border-b-white/0 dark:group-hover:border-b-white/100">
+            <FaExternalLinkAlt className="hidden group-hover:block text-lg" />
+            {title}
+          </h3>
           <p className="mt-2 leading-relaxed text-gray-700 dark:text-white/70">
             {description}
           </p>
@@ -51,7 +61,7 @@ export default function Project({
           quality={95}
           width={400}
           height={100}
-          className="absolute hidden sm:block top-8 -right-28 rounded-t-lg transition group-hover:scale-105 group-hover:-translate-x-3 group-hover:translate-y-3 group-hover:-rotate-2 group-even:group-hover:translate-x-3 group-even:group-hover:translate-y-3 group-even:group-hover:rotate-2 group-even:right-[initial] group-even:-left-32"
+          className="absolute hidden sm:block top-8 -right-28 rounded-t-lg transition group-hover:scale-110 group-hover:-translate-x-3 group-hover:translate-y-3 group-hover:-rotate-3 group-even:group-hover:translate-x-3 group-even:group-hover:translate-y-3 group-even:group-hover:rotate-3 group-even:right-[initial] group-even:-left-32"
         />
       </section>
     </motion.div>
